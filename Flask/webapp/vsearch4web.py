734a0aa0d99459d2db1089fb35fc75e45e86aa88
na-toyoshima <na.toyoshim@gmail.com>
2020-06-28 12:56:18 +0900

    finish p244

--- Flask/webapp/vsearch4web.py
+++ Flask/webapp/vsearch4web.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = "検索結果"
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> str:
-    return render_template('entry.html', the_title = 'web版のsearch4lettersへようこそ')
+    return render_template('entry.html', the_title='web版のsearch4lettersへようこそ')
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
